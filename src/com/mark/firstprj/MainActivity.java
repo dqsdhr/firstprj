1374b5d5ee4db36efb0b86fa4617a4795931a32a
Mark <dqsdhr@gmail.com>
2014-04-28 14:55:42 +0800

    再次尝试提交

--- src/com/mark/firstprj/MainActivity.java
+++ src/com/mark/firstprj/MainActivity.java
@@ -1,15 +1,42 @@
 package com.mark.firstprj;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	private TextView textView;
+	private Button button;
+	private boolean isClick = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		textView = (TextView) findViewById(R.id.textView);
+		button = (Button) findViewById(R.id.button);
+
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (isClick) {
+					textView.setText(R.string.hello);
+					isClick = false;
+				} else {
+					textView.setText("Hello Cool Job");
+					isClick = true;
+				}
+
+			}
+		});
+
 	}
 
 	@Override
